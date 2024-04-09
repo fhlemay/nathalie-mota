618bd3bd325225d22424dfbe656bfe198a7b5ce3
flemay <flemay@gmail.com>
2024-04-09 22:30:05 +0200

    Tester la mise en forme des menus (css)
    Corriger css de l'appel de la police

--- functions.php
+++ functions.php
@@ -5,8 +5,9 @@ add_theme_support('title-tag'); // ajouter auto. le titre du site dans l'en-têt
 
 function nathalie_mota_custom_styles()
 {
-  wp_enqueue_style('nathalie-mota-style', get_stylesheet_uri()); // charge style.css
+  wp_enqueue_style('nathalie-mota-fonts', get_stylesheet_directory_uri() . '/assets/css/fonts.css');
   wp_enqueue_style('normalize_style', get_stylesheet_directory_uri() . '/assets/css/normalize.css');
+  wp_enqueue_style('nathalie-mota-style', get_stylesheet_uri()); // charge style.css
 }
 add_action('wp_enqueue_scripts', 'nathalie_mota_custom_styles');
 
@@ -14,11 +15,3 @@ register_nav_menus(array(
   'main' => 'Menu Principal',
   'footer' => 'Bas de page',
 ));
-
-/*
-* Charger les styles référant aux polices de Google installées localement.
-*/
-function gfonts_scripts() {
-  wp_enqueue_style( 'space-mono-font', get_stylesheet_directory_uri() . '/assets/css/fonts.css' );
-}
-add_action( 'wp_enqueue_scripts', 'gfonts_scripts' );
