8b18353ebebb70b3e4ea8a3b7253b13046376693
flemay <flemay@gmail.com>
2024-04-09 15:24:41 +0200

    Ajouter normalize.css (css reset)

--- functions.php
+++ functions.php
@@ -2,3 +2,9 @@
 
 add_theme_support('post-thumbnails'); // prise en charge des images mises en avant
 add_theme_support('title-tag'); // ajouter auto. le titre du site dans l'en-tête
+
+function nathalie_mota_custom_styles()
+{
+  wp_enqueue_style('normalize_style', get_stylesheet_directory_uri() . '/assets/css/normalize.css');
+}
+add_action('wp_enqueue_scripts', 'nathalie_mota_custom_styles');
