ba133c373d70a2ab27516609cd8a3d91e001f9ae
flemay <flemay@gmail.com>
2024-04-10 17:04:42 +0200

    Ajouter le formulaire (bidon) à la modale

--- parts/modal-contact.php
+++ parts/modal-contact.php
@@ -2,7 +2,7 @@
   <!-- Modal content -->
   <div class="modal-content">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/contact-header-desktop.svg" alt="Contact">
-    <?php echo do_shortcode('[your_contact_form_shortcode]'); // Replace with your contact form shortcode 
+    <?php echo do_shortcode('[contact-form-7 id="ae91672" title="Contact"]'); // Replace with your contact form shortcode 
     ?>
   </div>
 </div>
