96f175a493e7bf353a5d2045b188403e82084ce4
flemay <flemay@gmail.com>
2024-04-10 13:05:26 +0200

    Ajouter fonction qui append texte à la fin du menu du footer

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ add_theme_support('title-tag'); // ajouter auto. le titre du site dans l'en-têt
 
 function nathalie_mota_custom_styles()
 {
-  wp_enqueue_style('normalize_style', get_stylesheet_directory_uri() . '/assets/css/normalize.css');
+  wp_enqueue_style('normalize-style', get_stylesheet_directory_uri() . '/assets/css/normalize.css');
   wp_enqueue_style('nathalie-mota-fonts', get_stylesheet_directory_uri() . '/assets/css/fonts.css');
   wp_enqueue_style('nathalie-mota-variables', get_stylesheet_directory_uri() . '/assets/css/variables.css');
   wp_enqueue_style('nathalie-mota-site-widths', get_stylesheet_directory_uri() . '/assets/css/site-widths.css');
@@ -18,3 +18,17 @@ register_nav_menus(array(
   'main' => 'Menu Principal',
   'footer' => 'Bas de page',
 ));
+
+function add_text_to_footer_nav_menu($items, $args)
+{
+  if ($args->theme_location == 'footer') {
+    // Add your custom content here. You can use HTML to format it.
+    $custom_text = '<li class="menu-item menu-item-type-custom menu-item-object-custom">Tous droits réservés</li>';
+
+    // Append the custom text to the menu items
+    $items .= $custom_text;
+  }
+
+  return $items;
+}
+add_filter('wp_nav_menu_items', 'add_text_to_footer_nav_menu', 10, 2);
