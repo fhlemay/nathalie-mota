e2a56adf032ed194792f3e908b25ce537134321e
flemay <flemay@gmail.com>
2024-04-10 11:47:53 +0200

    Ajouter variables.css

--- functions.php
+++ functions.php
@@ -5,9 +5,11 @@ add_theme_support('title-tag'); // ajouter auto. le titre du site dans l'en-têt
 
 function nathalie_mota_custom_styles()
 {
-  wp_enqueue_style('nathalie-mota-fonts', get_stylesheet_directory_uri() . '/assets/css/fonts.css');
   wp_enqueue_style('normalize_style', get_stylesheet_directory_uri() . '/assets/css/normalize.css');
-  wp_enqueue_style('nathalie-mota-style', get_stylesheet_uri()); // charge style.css
+  wp_enqueue_style('nathalie-mota-fonts', get_stylesheet_directory_uri() . '/assets/css/fonts.css');
+  wp_enqueue_style('nathalie-mota-variables', get_stylesheet_directory_uri() . '/assets/css/variables.css');
+  wp_enqueue_style('nathalie-mota-menus', get_stylesheet_directory_uri() . '/assets/css/menus.css');
+  // wp_enqueue_style('nathalie-mota-style', get_stylesheet_uri()); // charge style.css
 }
 add_action('wp_enqueue_scripts', 'nathalie_mota_custom_styles');
 
