6c0473595fe39331c087c5c4544da3f05e44b68d
flemay <flemay@gmail.com>
2024-04-18 17:48:16 +0200

    WIP : single-photo.php en cours de dev

--- single-photo.php
+++ single-photo.php
@@ -1,3 +1,21 @@
 <?php get_header(); ?>
-<h1>Nathalie Mota : page-photo.php</h1>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <h1><?php the_title(); ?></h1>
+        <div>
+          <?php the_content(); ?>
+          <?php 
+          if ( has_post_thumbnail() ) {
+    the_post_thumbnail('large');
+}
+          ?>
+<?php the_terms( $post->ID, 'categorie', 'Catégories: ', ', ' ); ?>
+<?php the_terms( $post->ID, 'format', 'Format: ', ', ' ); ?>
+          
+          <p>Custom fields (meta)</p>
+          <p><strong>Type:</strong> <?php echo get_post_meta(get_the_ID(), 'type', true); ?></p>
+          <p><strong>Référence:</strong> <?php echo get_post_meta(get_the_ID(), 'reference', true); ?></p>
+        </div>
+<?php endwhile; endif; ?>
+
 <?php get_footer(); ?>
