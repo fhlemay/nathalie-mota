27a441816892dbea6850e9a4ec5a5213242c4750
flemay <flemay@gmail.com>
2024-04-20 09:20:09 +0200

    WIP single-photo.php with tailwind-css

--- single-photo.php
+++ single-photo.php
@@ -1,21 +1,24 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <h1><?php the_title(); ?></h1>
-        <div>
-          <?php the_content(); ?>
-          <?php 
-          if ( has_post_thumbnail() ) {
-    the_post_thumbnail('large');
-}
-          ?>
-<?php the_terms( $post->ID, 'categorie', 'Catégories: ', ', ' ); ?>
-<?php the_terms( $post->ID, 'format', 'Format: ', ', ' ); ?>
-          
-          <p>Custom fields (meta)</p>
-          <p><strong>Type:</strong> <?php echo get_post_meta(get_the_ID(), 'type', true); ?></p>
-          <p><strong>Référence:</strong> <?php echo get_post_meta(get_the_ID(), 'reference', true); ?></p>
-        </div>
-<?php endwhile; endif; ?>
+    <h1><?php the_title(); ?></h1>
+    <div>
+    </div>
+    <div class="grid grid-cols-2 gap-2">
+      <section class="">
+
+        <?php the_terms(get_the_ID(), 'categorie', 'Catégories: ', ', '); ?>
+        <?php the_terms(get_the_ID(), 'format', 'Format: ', ', '); ?>
+        <?php get_post_meta(get_the_ID(), 'type', true); ?></p>
+        <p><strong>Référence:</strong> <?php echo get_post_meta(get_the_ID(), 'reference', true); ?></p>
+      </section>
+      <section class="">
+        <?php has_post_thumbnail() ? the_post_thumbnail('large') : ''; ?>
+      </section>
+      <section class=""></section>
+      <section class=""></section>
+    </div>
+<?php endwhile;
+endif; ?>
 
 <?php get_footer(); ?>
