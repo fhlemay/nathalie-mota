24c4713ddf3912d5e01734b6c21ddd199e6b887f
flemay <flemay@gmail.com>
2024-04-20 08:02:25 +0200

    Installer tailwind-css

--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@ function nathalie_mota_custom_styles()
   wp_enqueue_style('nathalie-mota-site-widths', get_stylesheet_directory_uri() . '/assets/css/site-widths.css');
   wp_enqueue_style('nathalie-mota-menus', get_stylesheet_directory_uri() . '/assets/css/menus.css');
   wp_enqueue_style('nathalie-mota-modal-contact', get_stylesheet_directory_uri() . '/assets/css/modal-contact.css');
+  wp_enqueue_style('nathalie-mota-tailwind', get_stylesheet_directory_uri() . '/assets/css/tailwind.css');
   // wp_enqueue_style('nathalie-mota-style', get_stylesheet_uri()); // charge style.css
 }
 add_action('wp_enqueue_scripts', 'nathalie_mota_custom_styles');
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  // content: ["./**/*.php"],
+  content: ["./single-photo.php"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
