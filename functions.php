438d0e69260a9fd06d784863a36507db0089a110
flemay <flemay@gmail.com>
2024-04-27 00:00:02 +0200

    Ne plus charger des feuilles de style inutiles

--- functions.php
+++ functions.php
@@ -12,34 +12,34 @@ add_theme_support('title-tag');  // ajouter auto. le titre du site dans l'en-tê
 
 function nathalie_mota_custom_styles()
 {
-    wp_enqueue_style('nathalie-mota-style', get_stylesheet_uri());  // charge style.css
-    wp_enqueue_style('nathalie-mota-fonts', get_stylesheet_directory_uri() . '/assets/css/fonts.css');
-    wp_enqueue_style('nathalie-mota-variables', get_stylesheet_directory_uri() . '/assets/css/variables.css');
-    wp_enqueue_style('nathalie-mota-site-widths', get_stylesheet_directory_uri() . '/assets/css/site-widths.css');
-    wp_enqueue_style('nathalie-mota-menus', get_stylesheet_directory_uri() . '/assets/css/menus.css');
-    wp_enqueue_style('nathalie-mota-modal-contact', get_stylesheet_directory_uri() . '/assets/css/modal-contact.css');
-    wp_enqueue_style('nathalie-mota-tailwind', get_stylesheet_directory_uri() . '/assets/css/tailwind.css');
+  wp_enqueue_style('nathalie-mota-style', get_stylesheet_uri());  // charge style.css
+  wp_enqueue_style('nathalie-mota-fonts', get_stylesheet_directory_uri() . '/assets/css/fonts.css');
+  // wp_enqueue_style('nathalie-mota-variables', get_stylesheet_directory_uri() . '/assets/css/variables.css');
+  // wp_enqueue_style('nathalie-mota-site-widths', get_stylesheet_directory_uri() . '/assets/css/site-widths.css');
+  // wp_enqueue_style('nathalie-mota-menus', get_stylesheet_directory_uri() . '/assets/css/menus.css');
+  // wp_enqueue_style('nathalie-mota-modal-contact', get_stylesheet_directory_uri() . '/assets/css/modal-contact.css');
+  wp_enqueue_style('nathalie-mota-tailwind', get_stylesheet_directory_uri() . '/assets/css/tailwind.css');
 }
 
 add_action('wp_enqueue_scripts', 'nathalie_mota_custom_styles');
 
 function nathalie_mota_scripts()
 {
-    // wp_enqueue_script('nathalie-mota-modal-contact-script', get_stylesheet_directory_uri() . '/assets/js/modal-contact.js');
-    // wp_enqueue_script('nathalie-mota-mobile-menu-script', get_stylesheet_directory_uri() . '/assets/js/mobile-menu.js');
-    wp_localize_script('nathalie-mota-mobile-menu-script', 'variables', array(
-        'themeUrl' => get_stylesheet_directory_uri(),
-    ));
+  // wp_enqueue_script('nathalie-mota-modal-contact-script', get_stylesheet_directory_uri() . '/assets/js/modal-contact.js');
+  // wp_enqueue_script('nathalie-mota-mobile-menu-script', get_stylesheet_directory_uri() . '/assets/js/mobile-menu.js');
+  wp_localize_script('nathalie-mota-mobile-menu-script', 'variables', array(
+    'themeUrl' => get_stylesheet_directory_uri(),
+  ));
 }
 
 add_action('wp_enqueue_scripts', 'nathalie_mota_scripts');
 
 function register_my_menus()
 {
-    register_nav_menus(array(
-        'main' => 'Menu Principal',
-        'footer' => 'Bas de page',
-    ));
+  register_nav_menus(array(
+    'main' => 'Menu Principal',
+    'footer' => 'Bas de page',
+  ));
 }
 
 add_action('init', 'register_my_menus');
@@ -68,12 +68,12 @@ add_filter('timber/context', 'add_to_context');
  */
 function add_to_context($context)
 {
-    // So here you are adding data to Timber's context object, i.e...
-    $context['foo'] = 'I am some other typical value set in your functions.php file, unrelated to the menu';
+  // So here you are adding data to Timber's context object, i.e...
+  $context['foo'] = 'I am some other typical value set in your functions.php file, unrelated to the menu';
 
-    // Now, in similar fashion, you add a Timber Menu and send it along to the context.
-    $context['main_menu'] = Timber::get_menu('main');
-    $context['footer_menu'] = Timber::get_menu('footer');
+  // Now, in similar fashion, you add a Timber Menu and send it along to the context.
+  $context['main_menu'] = Timber::get_menu('main');
+  $context['footer_menu'] = Timber::get_menu('footer');
 
-    return $context;
+  return $context;
 }
