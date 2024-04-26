a3096c93c24a783d0f6cac21edd3206185a7947b
flemay <flemay@gmail.com>
2024-04-26 13:20:58 +0200

    Charger style.css

--- functions.php
+++ functions.php
@@ -12,13 +12,13 @@ add_theme_support('title-tag');  // ajouter auto. le titre du site dans l'en-tê
 
 function nathalie_mota_custom_styles()
 {
+    wp_enqueue_style('nathalie-mota-style', get_stylesheet_uri());  // charge style.css
     wp_enqueue_style('nathalie-mota-fonts', get_stylesheet_directory_uri() . '/assets/css/fonts.css');
     wp_enqueue_style('nathalie-mota-variables', get_stylesheet_directory_uri() . '/assets/css/variables.css');
     wp_enqueue_style('nathalie-mota-site-widths', get_stylesheet_directory_uri() . '/assets/css/site-widths.css');
     wp_enqueue_style('nathalie-mota-menus', get_stylesheet_directory_uri() . '/assets/css/menus.css');
     wp_enqueue_style('nathalie-mota-modal-contact', get_stylesheet_directory_uri() . '/assets/css/modal-contact.css');
     wp_enqueue_style('nathalie-mota-tailwind', get_stylesheet_directory_uri() . '/assets/css/tailwind.css');
-    // wp_enqueue_style('nathalie-mota-style', get_stylesheet_uri()); // charge style.css
 }
 
 add_action('wp_enqueue_scripts', 'nathalie_mota_custom_styles');
