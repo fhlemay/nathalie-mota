e49b7cdabee09b94cf34dde824fd4b551f531ea6
flemay <flemay@gmail.com>
2024-04-29 22:21:28 +0200

    Intégrer la modale de contact avec template twig et logique Alpine.js

--- parts/modal-contact.php
+++
@@ -1,8 +0,0 @@
-<div id="contactModal" class="modal">
-  <!-- Modal content -->
-  <div class="modal-content">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/contact-header-desktop.svg" alt="Contact">
-    <?php echo do_shortcode('[contact-form-7 id="ae91672" title="Contact"]'); // Replace with your contact form shortcode 
-    ?>
-  </div>
-</div>
