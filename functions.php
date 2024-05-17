8ad513ecb1ffa39b8d726c765d8054976dd0770a
flemay <flemay@gmail.com>
2024-05-17 22:37:25 +0200

    inc : theme-support

--- functions.php
+++ functions.php
@@ -14,8 +14,6 @@ foreach ($files as $file) {
     }
 }
 
-add_theme_support('post-thumbnails');  // prise en charge des images mises en avant
-add_theme_support('title-tag');  // ajouter auto. le titre du site dans l'en-tête
 
 function nathalie_mota_custom_styles()
 {
---
+++ inc/05-theme-support.php
@@ -0,0 +1,4 @@
+<?php
+
+add_theme_support('post-thumbnails');  // prise en charge des images mises en avant
+add_theme_support('title-tag');  // ajouter auto. le titre du site dans l'en-tête
