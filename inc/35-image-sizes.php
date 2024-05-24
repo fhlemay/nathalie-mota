9a14d1cc626fb0c15c1b2fb786a7a6dc9ef075a5
flemay <flemay@gmail.com>
2024-05-24 14:30:37 +0200

    images : photo-card : Créer et utiliser taille image adaptée

--- inc/35-image-sizes.php
+++ inc/35-image-sizes.php
@@ -1,35 +1,5 @@
 <?php
 // Images sizes
-
-
-
-// Tailles de base de WordPress
-// update_option('thumbnail_size_w', 150);
-// update_option('thumbnail_size_h', 150);
-// update_option('medium_size_w', 300);
-// update_option('medium_size_h', 300);
-// update_option('large_size_w', 768);
-// update_option('large_size_h', 768);
-
-/* function disable_default_image_sizes() {
-    // Set thumbnail dimensions to zero
-    update_option('thumbnail_size_w', 0);
-    update_option('thumbnail_size_h', 0);
-
-    // Set medium dimensions to zero
-    update_option('medium_size_w', 0);
-    update_option('medium_size_h', 0);
-
-    // Set large dimensions to zero
-    update_option('large_size_w', 0);
-    update_option('large_size_h', 0);
-
-    // Optionally, set medium_large dimensions to zero (used in responsive images)
-    update_option('medium_large_size_w', 0);
-    update_option('medium_large_size_h', 0);
-}
-add_action('init', 'disable_default_image_sizes'); */
-
 function remove_default_image_sizes($sizes) {
     unset($sizes['thumbnail']);   // Remove Thumbnail size
     unset($sizes['medium']);      // Remove Medium size
@@ -42,9 +12,10 @@ function remove_default_image_sizes($sizes) {
 add_filter('intermediate_image_sizes_advanced', 'remove_default_image_sizes');
 
 function my_custom_sizes() {
-    // add_image_size('custom-size', 800, 600, true); // 800 pixels wide by 600 pixels tall, hard crop mode
     add_image_size('hero-desktop', 1440, 1440, false);
     add_image_size('hero-mobile', 375, 375, false);
+    add_image_size('photo-card-desktop', 566, 495, true);
+    add_image_size('photo-card-mobile', 318, 279, true);
 }
 add_action('after_setup_theme', 'my_custom_sizes');
 
