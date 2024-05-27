28e997525e78b709e1329bbb3622a70ed7bcfa5e
flemay <flemay@gmail.com>
2024-05-27 13:12:29 +0200

    cont & views : Ajouter et bouger des pages

--- controllers/single.php
+++ controllers/single.php
@@ -1,14 +1,3 @@
-<!-- <!DOCTYPE html>
-<html>
-
-<head></head>
-
-<body>
-  <h1>Nathalie Mota : single.php</h1>
-</body>
-
-</html> -->
-
 <?php
 
 use Timber\Timber;
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+use Timber\Timber;
+
+$current_template = pathinfo(__FILE__, PATHINFO_FILENAME) . '.twig';
+
+$context = Timber::context();
+
+Timber::render($current_template, $context);
